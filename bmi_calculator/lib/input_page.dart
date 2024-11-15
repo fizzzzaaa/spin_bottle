a49d735d61396f18c1bf6a11aed6ce75c4762ae9
Fiza Ahmad <141174715+fizzzzaaa@users.noreply.github.com>
2024-11-15 19:09:02 +0500

    lctr 38
    added RepeatRefactorCode

---
+++ bmi_calculator/lib/input_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'IconText.dart';
+
+
+const activeColor = Color(0xFF1D1E33);
+const deActivColor = Color(0xFF111328);
+
+
+class InputPage extends StatefulWidget {
+  @override
+  InputPageState createState() => InputPageState();
+}
+
+class InputPageState extends State<InputPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ), // AppBar
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RepeatRefactorCode(
+
+                    colors: (0xFF1123);
+      cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.person,
+                      label: "Male",
+                    ),
+                  ),
+
+                ),
+                Expanded(
+                  child: RepeatRefactorCode(
+                    onPressed: (){
+                      setState(() {
+                        selectGender=Gender.female;
+                      });
+                    },
+                    colors:selectGender==Gender.female?activeColor:deActivColor,
+                    cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.female,
+                      label: "Female",
+                    ),),
+
+
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child:RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+                  ),
+                ),
+                Expanded(
+                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ); //
+  }
+}
