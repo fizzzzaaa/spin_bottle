1da446d5ad4eac046bb70b0cd3921ff9b037f58f
Fiza Ahmad <141174715+fizzzzaaa@users.noreply.github.com>
2024-11-15 19:25:18 +0500

    lctr 42
    I've used enum with the name gender

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -2,29 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconText.dart';
 import 'ContainerFile.dart';
-class RefactorTextandIcon extends StatelessWidget {
-  RefactorTextandIcon({required this.iconData,required this.label});
-  final IconData iconData;
-  final String label;
+
+const activeColor = Color(0xFF1D1E33);
+const deActivColor = Color(0xFF111328);
+enum gender{
+  male,
+  female,
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  InputPageState createState() => InputPageState();
+}
+
+class InputPageState extends State<InputPage> {
+  gender? selectGender;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            iconData,
-            size: 80.0,
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Text(label,style: TextStyle(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ), // AppBar
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RepeatRefactorCode(
+                    onPressed: (){
+                      setState(() {
+                        selectGender=gender.male;
+                      });
+                    },
+                    colors: selectGender==gender.male?activeColor:deActivColor,
+                    cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.person,
+                      label: "Male",
+                    ),
+                  ),
 
-              fontSize: 18.0
-          ))
-        ]
+                ),
+                Expanded(
+                  child: RepeatRefactorCode(
+                    onPressed: (){
+                      setState(() {
+                        selectGender=gender.female;
+                      });
+                    },
+                    colors:selectGender==gender.female?activeColor:deActivColor,
+                    cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.female,
+                      label: "Female",
+                    ),),
 
-    );
+
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child:RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+                  ),
+                ),
+                Expanded(
+                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33), onPressed: () {  },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ); //
   }
 }
