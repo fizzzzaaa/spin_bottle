8003c69f96f1434439edba5948ca63b312449482
Fiza Ahmad <141174715+fizzzzaaa@users.noreply.github.com>
2024-11-15 19:30:46 +0500

    lctr 44
    use of function onPressed when tap the widget

--- bmi_calculator/lib/ContainerFile.dart
+++ bmi_calculator/lib/ContainerFile.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class RepeatRefactorCode extends StatelessWidget {
-  RepeatRefactorCode({required this.colors, this.cardwidget, required  this.onPressed});
+
+  final VoidCallback onPressed;
   final Color colors;
   final Widget? cardwidget;
-  final VoidCallback onPressed;
+  RepeatRefactorCode({required this.colors, this.cardwidget, required  this.onPressed});
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
